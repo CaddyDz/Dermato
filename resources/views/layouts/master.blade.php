fae538323ed8fb3b497fcaf045acedc450c1bca3
Salim Djerbouh <caddydz4@gmail.com>
2019-06-15 02:35:47 +0100

    cleared console errors
    * removed queryloader console error message.
    * conditionally remove child element.
    * link to root owl carousel.
    
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -94,7 +94,7 @@
     <script type='text/javascript' src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
         crossorigin="anonymous"></script>
-    <script type='text/javascript' src="js/owl.carousel.js"></script>
+    <script type='text/javascript' src="/js/owl.carousel.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.0.2/wow.min.js" integrity="sha256-kUFT0whnCzBMw9FnuCvtn35qOSPnrp7aRv6H2skEDIY="
         crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/stellar.js/0.6.2/jquery.stellar.min.js" integrity="sha256-aQ6KGDKk7w8XQNZsQaQnbfeC//XPUIphSp/X/ZEwtV0="
